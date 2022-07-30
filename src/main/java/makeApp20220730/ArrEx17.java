e01d8e0bf723fd5765512c1f48c38229eaa4fc61
sulin <hansulin922@gmail.com>
2022-07-30 20:09:17 +0900

    향상된 for문에 인덱스가 필요한 경우

--- src/main/java/makeApp20220730/ArrEx17.java
+++ src/main/java/makeApp20220730/ArrEx17.java
@@ -3,12 +3,12 @@ package makeApp20220730;
 public class ArrEx17 {
 
 	public static void main(String[] args) {
-		// 인덱스가 필요한 경우
+		// 향상된 for문에 인덱스가 필요한 경우
 
 		String[] names = {"토리", "풀이", "모모"};
 		int[] weight = {5, 3, 6};
 		
-		int i = 0; //인덱스
+		int i = 0; //인덱스로 사용할 변수 선언
 		
 		for (String name : names) {
 			System.out.println(name+" : "+weight[i]+"kg");
